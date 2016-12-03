2ff2ab34013caf75996327de16a833af9d724ffe
srujangopu <srujanjack@gmail.com>
2016-12-03 15:01:46 -0600

    updating user profile

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,96 +1,118 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $cordovaGeolocation) {
+  .controller('DashCtrl', function ($scope, $cordovaGeolocation, GOOGLE_CONFIG) {
 
-  var options = {timeout: 10000, enableHighAccuracy: true};
+    var options = {timeout: 10000, enableHighAccuracy: true};
+    $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      var mapOptions = {
+        center: latLng,
+        zoom: 17,
+        disableDefaultUI: true,
+        styles: GOOGLE_CONFIG,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+      //draw circle
+      $scope.circle = new google.maps.Circle({
+        map: $scope.map,
+        radius: 150,  // IN METERS.
+        fillColor: '#80dfff',
+        fillOpacity: 0.3,
+        strokeColor: "#FFF",
+        strokeWeight: 0,
+        center: latLng
+      });
 
-    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    var mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }, function (error) {
+      console.log("Could not get location");
+    });
+
+
+  })
+  .controller('ChatsCtrl', function ($scope, Chats) {
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+      Chats.remove(chat);
+    };
+  })
+
+  .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+  })
+
+  .controller('AccountCtrl', function ($scope, $state, $ionicActionSheet) {
+    $scope.settings = {
+      enableFriends: true
     };
 
-    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    $scope.logout = function () {
+      var hideSheet = $ionicActionSheet.show({
+        destructiveText: 'Logout',
+        titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
+        cancelText: 'Cancel',
+        cancel: function () {
+        },
+        buttonClicked: function (index) {
+          return true;
+        },
+        destructiveButtonClicked: function () {
+          //facebook logout
+          facebookConnectPlugin.logout(function () {
+              console.log("logging out")
+              $scope.authResponse = undefined;
+              $state.go('login');
+            },
+            function (fail) {
+              console.log("logging out error")
+            });
+        }
+      });
+    }
 
-  }, function(error){
-    console.log("Could not get location");
-  });
+  })
 
-})
-.controller('ChatsCtrl', function($scope, Chats) {
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope, $state, $ionicActionSheet) {
-  $scope.settings = {
-    enableFriends: true
-  };
-
-  $scope.logout = function () {
-    var hideSheet = $ionicActionSheet.show({
-      destructiveText: 'Logout',
-      titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
-      cancelText: 'Cancel',
-      cancel: function () {
-      },
-      buttonClicked: function (index) {
-        return true;
-      },
-      destructiveButtonClicked: function () {
-        //facebook logout
-        facebookConnectPlugin.logout(function () {
-            console.log("logging out")
-            $scope.authResponse = undefined;
-            $state.go('login');
-          },
-          function (fail) {
-            console.log("logging out error")
-          });
-      }
-    });
-  }
-
-})
-
-  .controller('LoginCtrl', function ($scope, $state, $q, $ionicLoading) {
+  .controller('LoginCtrl', function ($scope, $state, FacebookCtrl, UserService) {
 
     //This is the success callback from the login method
-    var fbLoginSuccess = function(response) {
+    var fbLoginSuccess = function (response) {
       var authResponse = response.authResponse;
       console.log(authResponse);
-      $state.go('tab.dash');
+      //get facebook profile
+      FacebookCtrl.getFacebookProfileInfo(response.authResponse.authToken).then(function (profileInfo) {
+        //update user info
+        UserService.updateUserProfile(profileInfo);
+        $state.go('tab.dash');
+      })
     };
 
 
     //This is the fail callback from the login method
-    var fbLoginError = function(error){
+    var fbLoginError = function (error) {
       console.log('fbLoginError', error);
-      $ionicLoading.hide();
     };
 
     //This method is executed when the user press the "Login with facebook" button
-    $scope.login = function() {
+    $scope.login = function () {
 
       console.log("login called")
-      facebookConnectPlugin.getLoginStatus(function(success){
+      facebookConnectPlugin.getLoginStatus(function (success) {
         console.log(success)
-        if(success.status === 'connected'){
+        if (success.status === 'connected') {
           console.log('getLoginStatus', success.status);
-          $state.go('tab.dash');
+
+          //get facebook profile
+          FacebookCtrl.getFacebookProfileInfo(success.authToken).then(function (profileInfo) {
+            //update user info
+            UserService.updateUserProfile(profileInfo);
+            $state.go('tab.dash');
+          })
+
         } else {
           console.log('getLoginStatus', success.status);
           facebookConnectPlugin.login([], fbLoginSuccess, fbLoginError);
@@ -98,4 +120,25 @@ angular.module('starter.controllers', [])
       });
     };
 
-  });
+  })
+  .constant('GOOGLE_CONFIG', [{
+    "featureType": "landscape.natural",
+    "elementType": "geometry.fill",
+    "stylers": [{"visibility": "on"}, {"color": "#e0efef"}]
+  }, {
+    "featureType": "poi",
+    "elementType": "geometry.fill",
+    "stylers": [{"visibility": "on"}, {"hue": "#1900ff"}, {"color": "#c0e8e8"}]
+  }, {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{"lightness": 100}, {"visibility": "simplified"}]
+  }, {
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [{"visibility": "off"}]
+  }, {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [{"visibility": "on"}, {"lightness": 700}]
+  }, {"featureType": "water", "elementType": "all", "stylers": [{"color": "#7dcdcd"}]}]);
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,101 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'img/ben.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'img/max.png'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'img/adam.jpg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'img/perry.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'img/mike.png'
+    }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
         }
+        return null;
       }
-      return null;
+    };
+  })
+
+  .factory('UserService', function ($q) {
+    var UserService = {};
+
+    UserService.updateUserProfile = function(profileInfo){
+
+      var info = $q.defer();
+      firebase.database().ref('users/' + profileInfo.id).update({
+        displayName: profileInfo.name,
+        photoURL: "http://graph.facebook.com/" + profileInfo.id + "/picture?type=large",
+        token: profileInfo.id,
+        status: "active"
+      });
+      info.resolve();
+      return info.promise;
     }
-  };
-});
+
+    UserService.getUserProfile = function(userId){
+      var info = $q.defer();
+      firebase.database().ref('users/' + userId).once('value')
+        .then(function (userQueryRes) {
+          info.resolve(userQueryRes);
+        })
+      return info.promise;
+    }
+
+    return UserService;
+  })
+
+  .factory('FacebookCtrl', function ($http, $q) {
+
+    var FacebookCtrl = {};
+
+    FacebookCtrl.getFacebookProfileInfo = function (authToken) {
+      var info = $q.defer();
+
+      facebookConnectPlugin.api('/me?fields=email,name&access_token=' + authToken, ["public_profile"],
+        function (response) {
+          console.log("facebook response");
+          console.log(response);
+          info.resolve(response);
+        },
+        function (response) {
+          console.log(response);
+          info.reject(response);
+        }
+      );
+      return info.promise;
+    };
+    return FacebookCtrl;
+  });
