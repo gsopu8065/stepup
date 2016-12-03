1afb8fd279d65eac736d374e43a7a1ea77b77584
srujangopu <srujanjack@gmail.com>
2016-12-03 13:43:04 -0600

    authorizarion done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,8 +21,67 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function($scope, $state, $ionicActionSheet) {
   $scope.settings = {
     enableFriends: true
   };
-});
+
+  $scope.logout = function () {
+    var hideSheet = $ionicActionSheet.show({
+      destructiveText: 'Logout',
+      titleText: 'Are you sure you want to logout? This app is awsome so I recommend you to stay.',
+      cancelText: 'Cancel',
+      cancel: function () {
+      },
+      buttonClicked: function (index) {
+        return true;
+      },
+      destructiveButtonClicked: function () {
+        //facebook logout
+        facebookConnectPlugin.logout(function () {
+            console.log("logging out")
+            $scope.authResponse = undefined;
+            $state.go('login');
+          },
+          function (fail) {
+            console.log("logging out error")
+          });
+      }
+    });
+  }
+
+})
+
+  .controller('LoginCtrl', function ($scope, $state, $q, $ionicLoading) {
+
+    //This is the success callback from the login method
+    var fbLoginSuccess = function(response) {
+      var authResponse = response.authResponse;
+      console.log(authResponse);
+      $state.go('tab.dash');
+    };
+
+
+    //This is the fail callback from the login method
+    var fbLoginError = function(error){
+      console.log('fbLoginError', error);
+      $ionicLoading.hide();
+    };
+
+    //This method is executed when the user press the "Login with facebook" button
+    $scope.login = function() {
+
+      console.log("login called")
+      facebookConnectPlugin.getLoginStatus(function(success){
+        console.log(success)
+        if(success.status === 'connected'){
+          console.log('getLoginStatus', success.status);
+          $state.go('tab.dash');
+        } else {
+          console.log('getLoginStatus', success.status);
+          facebookConnectPlugin.login([], fbLoginSuccess, fbLoginError);
+        }
+      });
+    };
+
+  });
